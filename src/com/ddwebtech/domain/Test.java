035a9b4606e8e7a3c7ba147af180858c71b3f405
Dean Sheppard <sheppard.dean@gmail.com>
2010-11-03 20:19:07 -0400

    Test

---
+++ src/com/ddwebtech/domain/Test.java
@@ -0,0 +1,16 @@
+package com.ddwebtech.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dean
+ * Date: Nov 3, 2010
+ * Time: 8:11:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+
+    public static void main(String args[]) {
+        System.out.println("This is a test");
+    }
+}
